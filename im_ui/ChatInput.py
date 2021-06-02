5ecfb4be4c228311569bf5e91980087c20a541f8
dezhiShen <1179551960@qq.com>
2021-06-02 16:18:49 +0800

    mod(全局样式):修改样式

--- im_ui/ChatInput.py
+++ im_ui/ChatInput.py
@@ -1,5 +1,4 @@
 import sys
-import typing
 
 from im_instance.MessageSenderInstance import MessageSenderInstance
 from im_instance.Entity import Message
@@ -18,23 +17,21 @@ class ChatInput(QWidget):
     __text_edit = None
     __message_send_handler = None
 
-    def __init__(self, parent: typing.Optional[QWidget],
-                 message_send_handler: MessageSenderInstance):
+    def __init__(self, message_send_handler: MessageSenderInstance):
         super().__init__()
         self.__message_send_handler = message_send_handler
-        self.initUI(parent)
+        self.initUI()
 
-    def initUI(self, parent: QWidget):
+    def initUI(self):
         # toolbar
-        toolbar = QToolBar(parent)
+        toolbar = QToolBar()
         imageSvg = QtSvg.QSvgWidget("./assets/icons/tupian.svg")
-        imageSvg.setParent(parent)
         imageSvg.setMaximumSize(QSize(20, 20))
         toolbar.addWidget(imageSvg)
         self.__toolbar = toolbar
         # 按钮组
-        closeButton = QPushButton(parent=parent, text="关闭")
-        sendButton = QPushButton(parent=parent, text="发送")
+        closeButton = QPushButton("关闭")
+        sendButton = QPushButton("发送")
         sendButton.clicked.connect(self.send_button_click)
         bottonsHbox = QHBoxLayout()
         bottonsHbox.addStretch(1)
@@ -42,7 +39,7 @@ class ChatInput(QWidget):
         bottonsHbox.addWidget(sendButton)
         mainBox = QVBoxLayout()
         mainBox.addWidget(toolbar)
-        text_edit = QTextEdit(parent)
+        text_edit = QTextEdit()
         text_edit.setAcceptRichText(False)
         self.__text_edit = text_edit
         mainBox.addWidget(text_edit)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtCore import QSize
 from im_ui.ChatInput import ChatInput
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication
 from im_instance.Entity import Message
 from im_instance.MessageSenderInstance import MessageSenderInstance
 from PyQt5 import QtSvg
@@ -15,19 +15,14 @@ class TestSender(MessageSenderInstance):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    root_widget = QWidget()
-    root_widget.setStyleSheet("QToolBar{spacing:20px;}")
-
+    app.setStyleSheet("QToolBar{spacing:40px;}")
     # 输入框初始化
-    chat_input = ChatInput(parent=root_widget,
-                           message_send_handler=TestSender())
+    chat_input = ChatInput(message_send_handler=TestSender())
     # 自定义 toolbar的按钮
     fileSvg = QtSvg.QSvgWidget("./assets/icons/wenjian.svg")
     fileSvg.setMaximumSize(QSize(20, 20))
     chat_input.toolbar().addWidget(fileSvg)
-
     # 展示
-    root_widget.setWindowTitle("输入框示例")
-    root_widget.show()
+    chat_input.setWindowTitle("输入框示例")
+    chat_input.show()
     sys.exit(app.exec_())
