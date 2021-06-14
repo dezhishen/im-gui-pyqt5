ae2bdbe7cda1f44ab3fcdb6b53acdce3a46c84fd
dezhiShen <1179551960@qq.com>
2021-06-14 22:34:39 +0800

    feat(file):增加文件缓存

---
+++ tools/CacheUtil.py
@@ -0,0 +1,17 @@
+import shelve
+
+
+class _CacheUtil:
+    _db: shelve.DbfilenameShelf = None
+
+    def __init__(self) -> None:
+        self._db = shelve.open("message.cache")
+
+    def set(self, key: str, value: object):
+        self._db[key] = value
+
+    def get(self, key: str) -> object:
+        return self._db.get(key)
+
+
+CACHE_UTIL = _CacheUtil()
--- tools/FileUtil.py
+++ tools/FileUtil.py
@@ -1,8 +1,21 @@
+from tools.CacheUtil import CACHE_UTIL
+
+
 class FileUtil:
+
     def __init__(self):
         pass
 
     @staticmethod
-    def readQss(style):
+    def read_qss(style):
         with open(style, "r") as f:
             return f.read()
+
+    @staticmethod
+    def put_cache(key: str, path: str, file: bytes):
+        with open(path, 'wb') as fd:
+            fd.write(file)
+        CACHE_UTIL.set(key, path)
+
+    def get_cache_path(key: str) -> str:
+        return CACHE_UTIL.get(key)
