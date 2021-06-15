06c79643200599cc9dba2a2e7f12e0a0a37479a9
dezhiShen <1179551960@qq.com>
2021-06-15 10:32:31 +0800

    feat(util):增加下载网络文件

--- tools/FileUtil.py
+++ tools/FileUtil.py
@@ -1,4 +1,7 @@
+from tools.ThreadPoolUtil import THREAD_POOL
 from tools.CacheUtil import CACHE_UTIL
+import requests
+import uuid
 
 
 class FileUtil:
@@ -17,5 +20,19 @@ class FileUtil:
             fd.write(file)
         CACHE_UTIL.set(key, path)
 
+    @staticmethod
     def get_cache_path(key: str) -> str:
         return CACHE_UTIL.get(key)
+
+    @staticmethod
+    def _download_url(url: str, file_path: str):
+        r = requests.get(url)
+        with open(file_path, "wb") as f:
+            f.write(r.content)
+        CACHE_UTIL.set("file_cache:"+url, file_path)
+
+    @staticmethod
+    def get_storage_from_url(url: str, dir: str) -> str:
+        path = dir+"/"+uuid.uuid1()
+        THREAD_POOL.submit(FileUtil._download_url, url, path)
+        return path
